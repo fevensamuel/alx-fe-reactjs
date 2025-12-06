bbd913248dac996f2898a123288966d404fc6ddc
Feven <fevensamuelrahel@gmail.com>
2025-12-05 22:07:59 -1100

    new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', fontSize: '18px' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
