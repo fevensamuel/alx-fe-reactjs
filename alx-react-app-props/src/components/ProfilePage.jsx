c6d91ccbe8961137f4c68cd5e57178dcfb52589d
Feven <fevensamuelrahel@gmail.com>
2025-12-05 22:37:49 -1100

    Info

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,10 @@
+// src/ProfilePage.jsx
+
+import React from 'react';
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />; // No need to pass userData as prop anymore
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,10 @@
+// src/UserInfo.jsx
+
+import React from 'react';
+import UserDetails from './UserDetails';
+
+function UserInfo() {
+  return <UserDetails />; // No need to pass userData as prop anymore
+}
+
+export default UserInfo;
