51f032a6422cd2b6f25d9fc228836c645a72932d
Feven <fevensamuelrahel@gmail.com>
2025-12-10 12:11:58 -1100

    My Company

---
+++ my-company/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar'; // Import Navbar component
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar /> {/* Display navbar on all pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+	<Footer /> {/* Footer on all pages */}
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '20px', textAlign: 'center', background: '#333', color: 'white' }}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
