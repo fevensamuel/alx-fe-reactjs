b609b3c207148acc8eb7d6b26c02b6744486ede1
Feven <fevensamuelrahel@gmail.com>
2025-12-21 19:48:38 +0300

    Update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,34 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(
-    `https://api.github.com/users/${username}`
+const BASE_URL = "https://api.github.com/search/users?q=";
+
+export const searchUsers = async ({ username, location, minRepos, page = 1 }) => {
+
+  let query = username || "";
+
+  if (location) {
+    query += ` location:${location}`;
+  }
+
+  if (minRepos) {
+    query += ` repos:>=${minRepos}`;
+  }
+
+  const apiUrl = `${BASE_URL}${query}&per_page=10&page=${page}`;
+
+  const response = await axios.get(apiUrl);
+
+  // Fetch full user details (repos, location not in search results)
+  const usersWithDetails = await Promise.all(
+    response.data.items.map(async (user) => {
+      const userDetails = await axios.get(user.url);
+      return {
+        ...user,
+        location: userDetails.data.location,
+        public_repos: userDetails.data.public_repos,
+      };
+    })
   );
-  return response.data;
+
+  return usersWithDetails;
 };
